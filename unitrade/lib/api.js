66706b232796d9369b77f5d5acee76f8f193f7bc
Moudjahid Moussa <mm12515@nyu.edu>
2025-05-09 17:25:29 +0400

    changed files from the lib/supabase/listings to lib/api

--- unitrade/lib/api.js
+++ unitrade/lib/api.js
@@ -142,3 +142,62 @@ export const fetchProductDetails = async (productId) => {
     }
   }
 }
+
+
+export async function banListing(listingId) {
+  const supabase = createClient()
+  
+
+  const {error,data} = await supabase
+    .from("listings")
+    .update({
+      status: "banned",
+      banned_at: new Date().toISOString(),
+    })
+    .eq('id', listingId)
+    .select("id")
+    .maybeSingle()
+    
+    
+
+  if (error) {
+    console.error("banListing error:", error)
+    return { success: false, error: error.message }
+  }
+  if(!data){
+    
+    console.log("this is the received data, ",data)
+  }
+
+
+  return { success: true }
+}
+
+
+export async function unbanListing(listingId) {
+    const supabase = createClient()
+  
+    const { error, data } = await supabase
+      .from("listings")
+      .update({
+        status: "active",     
+        banned_at: null,
+      })
+      .eq("id", listingId)
+      .select("id")
+      .maybeSingle()
+  
+    if (error) {
+      console.error("unbanListing error:", error)
+      return { success: false, error: error.message }
+    }
+  
+    return { success: true }
+  }
+  
+  
+  
+  
+  
+  
+  
